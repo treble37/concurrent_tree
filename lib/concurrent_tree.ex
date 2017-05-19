ded58e5c9d922541ad6f64c34f68892c62712201
Bruce <brucepark0@gmail.com>
2017-05-19 19:32:39 -0400

    Initial mix application w/Elixir

---
+++ lib/concurrent_tree.ex
@@ -0,0 +1,18 @@
+defmodule ConcurrentTree do
+  @moduledoc """
+  Documentation for ConcurrentTree.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConcurrentTree.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/concurrent_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule ConcurrentTreeTest do
+  use ExUnit.Case
+  doctest ConcurrentTree
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
