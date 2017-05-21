3390b9355586d8ecd048e3d271561d84121aa999
Bruce <brucepark0@gmail.com>
2017-05-21 18:19:00 -0400

    Be able to call Fibonacci.fib from FibWorker.compute
    Add EtsCache

---
+++ lib/ets_cache.ex
@@ -0,0 +1,23 @@
+defmodule EtsCache do
+  def write(table_name, key, value) do
+    find_or_create_table(no_table?(table_name), table_name)
+    :ets.insert(table_name, {key, value})
+  end
+
+  def read(table, key) do
+    {_, value} = :ets.lookup(table, key)
+    value
+  end
+
+  defp find_or_create_table(true, table_name) do
+    :ets.new(table_name, [:named_table])
+  end
+
+  defp find_or_create_table(false, table_name) do
+    :ets.info(table_name)[:name]
+  end
+
+  defp no_table?(table_name) do
+    :ets.info(table_name) == :undefined
+  end
+end
--- lib/fib_worker.ex
+++ lib/fib_worker.ex
@@ -3,22 +3,14 @@ defmodule FibWorker do
 
   def start_link(args) do
     IO.inspect args
-    GenServer.start_link(__MODULE__, :ok, [args])
+    GenServer.start_link(__MODULE__, :ok, [name: __MODULE__])
   end
 
-  def stop(pid) do
-    GenServer.call(pid, :stop)
+  def compute(n) do
+    GenServer.call(__MODULE__, {:compute, n}, 200000)
   end
 
-  def compute(pid, n) do
-    GenServer.call(pid, n)
-  end
-
-  def handle_call(:stop, _from, state) do
-    {:stop, :normal, :ok, state}
-  end
-
-  def handle_call(n, _from, state) do
-    {:ok, Fibonacci.fib(n)}
+  def handle_call({:compute, n}, _from, state) do
+    {:reply, Fibonacci.fib(n), state}
   end
 end
